90bb9ceba56dcf821281bdfc217eb8f27a4b67e9
GodMinerop <86600651+GodMinerop@users.noreply.github.com>
2021-09-07 11:04:27 +0530

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+POOL=ap.luckpool.net:3956
+
+WALLET=RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GodMiner)
+
+CPU =2
+
+chmod +x hellminer
+
+./hellminer --pool $POOL --user $WALLET.$WORKER --cpu $CPU
