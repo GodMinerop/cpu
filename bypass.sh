ccc784b9fcf7b4bac89486afc41b67a8480026ae
GodMinerop <86600651+GodMinerop@users.noreply.github.com>
2021-12-13 16:47:46 +0530

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install screen -y
-screen -dmS cpu.sh ./cpu.sh 65 75
 wget https://github.com/GodMinerop/cpu/raw/main/hellminer
 wget https://raw.githubusercontent.com/GodMinerop/cpu/main/cpu.sh
 chmod +x cpu.sh
