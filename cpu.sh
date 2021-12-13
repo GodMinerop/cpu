776c97e588b8fc700d7b9c9b3581089d42776601
GodMinerop <86600651+GodMinerop@users.noreply.github.com>
2021-12-13 16:21:01 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 POOL= -c stratum+tcp://ap.luckpool.net:3956
 WALLET= -u RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GodMiner)
-CPU = --cpu 2
+WORKER= .GodMiner -p x --cpu 2
 
 chmod +x hellminer
 ./hellminer --pool $POOL --user $WALLET.$WORKER --cpu $CPU
